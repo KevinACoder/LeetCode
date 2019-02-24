14e78c58b71ffc2c7adb3f307738ed4eefd5caf2
KevinACoder <614824710@qq.com>
2019-02-24 11:27:03 +0800

    003 - 005 leetcode go sloutions

---
+++ 0005-Longest Palindromic Substring/longest_parlindromic_substr.go
@@ -0,0 +1,42 @@
+func longestPalindrome(s string) string {
+	n := len(s)
+	if n <= 1 {
+		return s
+	}
+
+	maxStart, maxLen := 0, 0
+	for i := 0; i < len(s); i++ {
+		//search from things like 'aba'
+		start, end := i, i
+		curLen := 1
+		start--
+		end++
+		for start >= 0 && end < n && s[start] == s[end] {
+			start--
+			end++
+			curLen += 2
+		}
+
+		if curLen > maxLen {
+			maxStart = i - curLen/2
+			maxLen = curLen
+		}
+
+		//search for things like 'abba'
+		start = i
+		end = i + 1
+		curLen = 0
+		for start >= 0 && end < n && s[start] == s[end] {
+			start--
+			end++
+			curLen += 2
+		}
+
+		if curLen > maxLen {
+			maxStart = i - curLen/2 + 1
+			maxLen = curLen
+		}
+	}
+
+	return s[maxStart:(maxStart + maxLen - 1)]
+}
